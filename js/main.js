20d95419c64e29288abe1f2914ed96117a6502d9
Nate Nguyen <nathannguyen555@gmail.com>
2024-01-28 13:42:08 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const $body = $("body");
+
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+const $favoritedStories = $("#favorited-stories");
+const $ownStories = $("#my-stories");
+const $storiesContainer = $("#stories-container");
+const $storiesLists = $(".stories-list");
+
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+
+const $submitForm = $("#submit-form");
+
+const $navSubmitStory = $("#nav-submit-story");
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+
+const $userProfile = $("#user-profile");
+
+function hidePageComponents() {
+  const components = [
+    $storiesLists,
+    $loginForm,
+    $submitForm,
+    $userProfile,
+    $signupForm,
+  ];
+  components.forEach(c => c.hide());
+}
+
+async function start() {
+  console.debug("start");
+
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+
+  if (currentUser) updateUIOnUserLogin();
+}
+
+console.warn("HEY STUDENT: This program sends many debug messages to" +
+  " the console. If you don't see the message 'start' below this, you're not" +
+  " seeing those helpful debug messages. In your browser console, click on" +
+  " menu 'Default Levels' and add Verbose");
+$(start);
